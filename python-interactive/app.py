0d8dab7d31c36028abcf84791a367d40c352cd8b
Adi Thakker <Aditya.Thakker@gmail.com>
2025-12-16 12:37:12 -0500

    Add initial implementation of app.py and workspace configuration

---
+++ python-interactive/app.py
@@ -0,0 +1,28 @@
+print("HelloWorld")
+
+
+def Say_Hello(first_name, last_name):
+    print(F"{first_name} - {last_name}")
+
+
+Say_Hello("Adi", "Thakker")
+
+
+discount = 10
+
+
+def Change_Discount(discount):
+    discount = 5
+    return discount
+
+
+result = Change_Discount(discount)
+result
+
+
+def Return_Tuple():
+    return "Hello", "World"
+
+
+greeting1, greeting2 = Return_Tuple()
+print()
